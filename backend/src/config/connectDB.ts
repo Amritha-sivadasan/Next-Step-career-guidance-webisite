d2e8c58f32ed574051836858e4782b7f86500fce
Amritha <amrithas172@gmail.com>
2024-09-07 16:07:40 +0530

    last change

--- backend/src/config/connectDB.ts
+++ backend/src/config/connectDB.ts
@@ -3,15 +3,17 @@ export let dbInstance: typeof mongoose;
 
 export default async () => {
   try {
+    console.log('mongourl ',process.env.MONGOURL)
     if (process.env.MONGOURL) {
-      dbInstance = await mongoose.connect(process.env.MONGOURL,{
-        tls: true,  
-        tlsAllowInvalidCertificates: true});
+      dbInstance = await mongoose.connect(process.env.MONGOURL, {
+        tls: true,
+        tlsAllowInvalidCertificates: true,
+      });
       console.log("mongodb connection successulll");
-    }else {
-        throw new Error("MONGODB_URL not defined")
+    } else {
+      throw new Error("MONGODB_URL not defined");
     }
   } catch (error) {
-    console.log('monogdb connection failed',error);
+    console.log("monogdb connection failed", error);
   }
 };
